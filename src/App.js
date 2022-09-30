034a0d0a918b2495a30d0dcf3335048539d838a5
Ibrahim Khurshid <ibrahimkhurshid@hotmail.com>
2022-09-30 15:59:56 +0500

    Basic layout created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import { hamburgersvg, filesvg, searchsvg, sourcecontrolsvg, profilesvg, settingsvg } from './assets/icons';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <div className='icons-col'>
+
+        <div className='icons-top'>
+          <div className='icon-container'>{hamburgersvg}</div>
+          <div className='icon-container'>{filesvg}</div>
+          <div className='icon-container'>{searchsvg}</div>
+          <div className='icon-container'>{sourcecontrolsvg}</div>
+          <div className='icon-container'>{filesvg}</div>
+          <div className='icon-container'>{filesvg}</div>
+          <div className='icon-container'>{filesvg}</div>
+        </div>
+
+        <div className='icons-bottom'>
+          <div className='icon-container'>{profilesvg}</div>
+          <div className='icon-container'>{settingsvg}</div>
+        </div>
+      </div>
+
+      <div className='files-col'>
+      </div>
+
+      <textarea className='body-col'>
+      </textarea>
     </div>
   );
 }
